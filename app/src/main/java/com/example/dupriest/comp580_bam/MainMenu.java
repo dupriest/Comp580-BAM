6ca5a47ba3b2d56df7886e8fc3f0c071fc4461ec
Megan DuPriest <dupriest.megan@gmail.com>
2017-03-22 21:30:34 -0400

    Framework for main menu and play activity. Play activity needs work on sizing that would adjust to screen size.

--- app/src/main/java/com/example/dupriest/comp580_bam/MainMenu.java
+++ app/src/main/java/com/example/dupriest/comp580_bam/MainMenu.java
@@ -1,7 +1,9 @@
 package com.example.dupriest.comp580_bam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    void play(View view)
+    {
+        Intent X = new Intent(this, game.class);
+        startActivity(X);
     }
 }
