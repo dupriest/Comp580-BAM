b16a7de8344487c4d64cb08d6eb3177fd61e4954
Megan DuPriest <dupriest.megan@gmail.com>
2017-04-18 13:05:12 -0400

    Delete button with confirm to delete added.

--- app/src/main/java/com/example/dupriest/comp580_bam/select.java
+++ app/src/main/java/com/example/dupriest/comp580_bam/select.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 public class select extends AppCompatActivity {
@@ -38,6 +39,17 @@ public class select extends AppCompatActivity {
 
     void delete(View view)
     {
+        TableLayout mainMenu = (TableLayout)findViewById(R.id.mainMenu);
+        TableLayout deleteMenu = (TableLayout)findViewById(R.id.deleteMenu);
+
+        mainMenu.setVisibility(mainMenu.INVISIBLE);
+        deleteMenu.setVisibility(deleteMenu.VISIBLE);
+        view.announceForAccessibility("Do you really want to delete this saved slot?  Select NO to cancel.");
+
+    }
+
+    public void yes(View view)
+    {
         Context context = getApplicationContext();
         SharedPreferences sharedPref2;
         if(slot.equals("SLOT 1"))
@@ -52,8 +64,7 @@ public class select extends AppCompatActivity {
         {
             sharedPref2 = context.getSharedPreferences(getString(R.string.slot3_file_key), Context.MODE_PRIVATE);
         }
-        Button back = (Button)findViewById(R.id.back);
-        back.setClickable(false);
+
         SharedPreferences.Editor editor = sharedPref2.edit();
         String key;
         for(int i = 0; i < 20; i++)
@@ -64,12 +75,28 @@ public class select extends AppCompatActivity {
             editor.putString(key, "empty");
         }
         editor.commit();
-        back.setClickable(true);
-        CharSequence text = "SLOT CLEARED!";
+
+        TableLayout mainMenu = (TableLayout)findViewById(R.id.mainMenu);
+        TableLayout deleteMenu = (TableLayout)findViewById(R.id.deleteMenu);
+
+        mainMenu.setVisibility(mainMenu.VISIBLE);
+        deleteMenu.setVisibility(deleteMenu.INVISIBLE);
+
+        CharSequence text = "SLOT DELETED!";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    public void no(View view)
+    {
+        TableLayout mainMenu = (TableLayout)findViewById(R.id.mainMenu);
+        TableLayout deleteMenu = (TableLayout)findViewById(R.id.deleteMenu);
+
+        mainMenu.setVisibility(mainMenu.VISIBLE);
+        deleteMenu.setVisibility(deleteMenu.INVISIBLE);
+        view.announceForAccessibility(slot + " not deleted");
     }
 
 }
